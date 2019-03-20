b69fa598a001f5bc96c56aa6593187929793a0db
emlivo1 <emlivo1@etf.unsa.ba>
2019-03-20 10:05:27 +0100

    immitialcommit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label hello;
+    public Label labela;
+
+    public void submit (ActionEvent actionEvent){
+        hello.setText("Hi!");
+    }
+
+    public void ispisi(ActionEvent actionEvent) {
+        labela.setText("hello world");
+    }
 }
